177a607125398502e9ce158eab768f7eaf64245c
Alvaro11G <agarcia11746@gmail.com>
2022-08-19 23:11:33 -0600

    initial commit

---
+++ Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String greeting = "Hello";
+            int number = 11;
+            char letter = 'A';
+            bool isCodeFun = true;
+            double bigNumber = 111;
+            decimal deci = 10.56M;
+
+            Console.WriteLine($"{greeting} int is {number}, char is {letter}, bool is {isCodeFun}," +
+                $"double is {bigNumber}, and float is {deci}");
+
+        }
+    }
+}
